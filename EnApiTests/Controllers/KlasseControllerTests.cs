e1e7d588474e5a31aa45fda935d237d89489ba63
Nikolaj04 <felsdd04@gmail.com>
2025-11-23 14:53:50 +0100

    1

---
+++ EnApiTests/Controllers/KlasseControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnApi.Controllers.Tests
+{
+    [TestClass()]
+    public class KlasseControllerTests
+    {
+        [TestMethod()]
+        public void KlasseControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
